323e2697ce38bca5910e92f7ff794189eecf8fce
Csaba Roszik <csaba_roszik@epam.com>
2020-01-17 10:42:52 +0100

    Better test case organisation

--- src/slices/postsSlice.test.ts
+++ src/slices/postsSlice.test.ts
@@ -1,33 +1,26 @@
 import postsSlice, { Post } from "../slices/postsSlice";
-import deepFreeze from "deep-freeze";
+import { AnyAction } from "redux";
 
 const sampleData: Post[] = require("./__mocks__/samplePosts.json");
-
-const getInitialState = (): Post[] => {
-  return deepFreeze([] as Post[]) as Post[];
-};
 
 describe("posts slice", () => {
   describe("receivePosts action", () => {
     it("should create an action to receive posts", () => {
       const actionCreator = postsSlice.actions.receivePosts;
-      const expectedAction = {
-        type: actionCreator.toString(),
-        payload: sampleData
-      };
-      expect(actionCreator(sampleData)).toEqual(expectedAction);
+      expect(actionCreator(sampleData)).toMatchSnapshot();
     });
   });
 
   describe("combined reducer", () => {
-    it("should return the initial state", () => {
-      expect(getInitialState()).toEqual([] as Post[]);
+    it("should return the expected default initial state", () => {
+      const initialState = postsSlice.reducer(undefined, {} as AnyAction);
+      expect(initialState).toEqual([] as Post[]);
     });
 
     it("should handle receivePosts action", () => {
       const reducer = postsSlice.reducer;
       const actionCreator = postsSlice.actions.receivePosts;
-      expect(reducer(getInitialState(), actionCreator(sampleData))).toEqual(
+      expect(reducer([] as Post[], actionCreator(sampleData))).toEqual(
         sampleData
       );
     });
