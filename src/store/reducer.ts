bb3f0bf1fcce391efaa6c82298062e05fc9ab7ec
Csaba Roszik <csaba_roszik@epam.com>
2020-01-17 10:43:36 +0100

    Leaner state organisation

--- src/store/reducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/redux-toolkit";
-import postsSlice from "../slices/postsSlice";
-
-const rootReducer = combineReducers({
-  posts: postsSlice.reducer
-});
-
-export default rootReducer;
--- src/store/store.test.tsx
+++
@@ -1,8 +0,0 @@
-import store from "./index";
-
-describe("State", () => {
-  it("should contain postsSlice", () => {
-    const { posts } = store.getState();
-    expect(posts).toEqual([]);
-  });
-});
