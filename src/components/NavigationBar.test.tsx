0e0f7f3b84bef265f217c9c27d3bbd0347935065
Csaba Roszik <csaba_roszik@epam.com>
2020-01-20 11:29:32 +0100

    Enzyme's shallow render usage.

--- src/components/NavigationBar.test.tsx
+++ src/components/NavigationBar.test.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import NavigationBar from "./NavigationBar";
-import { MemoryRouter } from "react-router-dom";
 
 describe("NavigationBar", () => {
   it("renders a navigation menu", () => {
-    const component = render(
-      <MemoryRouter>
-        <NavigationBar />
-      </MemoryRouter>
-    );
+    const component = shallow(<NavigationBar />);
     expect(component).toMatchSnapshot();
   });
 });
