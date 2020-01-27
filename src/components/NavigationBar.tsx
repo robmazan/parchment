9983888b6929a5132333afc208902d50dd98a35b
Csaba Roszik <csaba_roszik@epam.com>
2020-01-27 13:36:14 +0100

    Implement Emotion on NavigationBar component

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoginStatus from "./LoginStatus";
+import styled from "@emotion/styled";
+
+const textColor = "#61dafb";
+const backgroundColor = "#282c34";
 
 const NavigationBar: React.FC = props => {
+  const NavitationMenu = styled.ul`
+    background-color: ${backgroundColor};
+    list-style-type: none;
+    padding-inline-start: 0;
+    display: flex;
+    justify-content: center;
+    margin-block-end: 0;
+    margin-block-start: 0;
+    padding: 1em;
+  `;
+  const NavigationItem = styled.li`
+    margin: 0.3em;
+  `;
+  const NavigationLink = styled(Link)`
+    color: ${textColor};
+  `;
   return (
     <nav>
-      <ul className="nav-menu">
-        <li className="nav-menu__item">
-          <Link to="/" className="nav-menu__link">
+      <NavitationMenu>
+        <NavigationItem>
+          <NavigationLink to="/" className="nav-menu__link">
             Home
-          </Link>
-        </li>
-        <li className="nav-menu__item">
+          </NavigationLink>
+        </NavigationItem>
+        <NavigationItem>
           <LoginStatus loginURI="/login" logoutURI="/logout" />
-        </li>
-      </ul>
+        </NavigationItem>
+      </NavitationMenu>
     </nav>
   );
 };
