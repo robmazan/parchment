c42b4a3ac0ce575aa0e62a2e0a64a04d41c71f56
Csaba Roszik <csaba_roszik@epam.com>
2020-01-27 14:13:55 +0100

    Implement Emotion on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/core";
+
+const textColor = "#61dafb";
+const backgroundColor = "#282c34";
 
 const App: React.FC = () => {
+  const AppLogoSpinAnimation = keyframes`
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  `;
+  const AppHeader = styled.header`
+    background-color: ${backgroundColor};
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+  const AppLink = styled.a`
+    color: ${textColor};
+  `;
+  const AppLogo = styled.img`
+    height: 40vmin;
+    pointer-events: none;
+    @media (prefers-reduced-motion: no-preference) {
+      animation: ${AppLogoSpinAnimation} infinite 20s linear;
+    }
+  `;
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    <AppHeader>
+      <AppLogo src={logo} alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
-      <a
-        className="App-link"
+      <AppLink
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
       >
         Learn React
-      </a>
-    </header>
+      </AppLink>
+    </AppHeader>
   );
 };
 
